c07f599c3ec74feac43a71f5dedc3cbaa05299c5
Etherium <4huntbrown@gmail.com>
2026-03-25 15:29:42 +1000

    clean up

--- src/main/java/com/esuproject/Launcher.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.esuproject;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(com.esuproject.PetApplication.class, args);
-    }
-}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.esuproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.esuproject to javafx.fxml;
-    exports com.esuproject;
-}

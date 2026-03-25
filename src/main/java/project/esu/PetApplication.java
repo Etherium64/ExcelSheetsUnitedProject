a1eb5ce1f8e81f7751a94f0eaa3e80d0d2c16688
Etherium <4huntbrown@gmail.com>
2026-03-25 15:23:23 +1000

    testing

--- src/main/java/project/esu/PetApplication.java
+++ src/main/java/project/esu/PetApplication.java
@@ -7,13 +7,15 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class PetApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(PetApplication.class.getResource("Menu-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 720, 720);
         stage.setScene(scene);
         stage.show();
     }
+
+
 }

0babf1e933b1f0a5495eacb145acd60c7e694973
Etherium <4huntbrown@gmail.com>
2026-03-25 14:18:33 +1000

    formating

--- src/main/java/com/esuproject/Launcher.java
+++ src/main/java/com/esuproject/Launcher.java
@@ -1,9 +1,9 @@
-package com.esuproject;
+package main.java.com.esuproject;
 
 import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(PetApplication.class, args);
+        Application.launch(com.esuproject.PetApplication.class, args);
     }
 }

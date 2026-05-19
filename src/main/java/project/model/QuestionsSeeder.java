25e6e2d92d174b355c610131815f86abc8223143
daniel <daniel@Castor.lan>
2026-05-19 14:52:16 +1000

    overhaul: connect SQL tables with foreign key, implement scoreboard for trivia module, implement SHA512 authentication and login gui, refactor controller code

--- src/main/java/project/model/QuestionsSeeder.java
+++ src/main/java/project/model/QuestionsSeeder.java
@@ -31,7 +31,7 @@ public class QuestionsSeeder {
              PreparedStatement ps = conn.prepareStatement(sql)) {
 
             // Question 1
-            ps.setString(1, "What is the capital of France?");
+            ps.setString(1, "What is the capital ofgiFrance?");
             ps.setString(2, "London");
             ps.setString(3, "Berlin");
             ps.setString(4, "Paris");
